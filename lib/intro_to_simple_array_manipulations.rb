03e6a5b7cb1e579513d54baaaeba1273539890f7
Ana Riat <t.riat24@gmail.com>
2019-05-12 08:50:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,5 @@ describe "#using push.do"
 it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next country = "Niger"
-  
+  update_array 
   
