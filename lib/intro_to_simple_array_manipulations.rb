1ae78943ef687d6e97133ab59688198700144c27
Ana Riat <t.riat24@gmail.com>
2019-05-12 08:51:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,5 +4,6 @@ it "takes in two arguments, an array and a string and adds that string to the en
   next country = "Niger"
   update_array = using.push (countries_in_western_africa, next country)
   expected (updated_array.last).to eq(next country)
+end 
   
   
